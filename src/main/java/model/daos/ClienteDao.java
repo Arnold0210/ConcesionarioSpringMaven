6a086aa86d2694c2d9080d3c8c97261a05184bca
Arnold0210 <arnoldjulianherreraquiones@gmail.com>
2019-03-11 18:25:46 -0500

    add ClienteDao

---
+++ src/main/java/model/daos/ClienteDao.java
@@ -0,0 +1,49 @@
+package model.daos;
+
+import model.dtos.Cliente;
+
+public class ClienteDao implements model.daos.interfaces.ClienteDao{
+
+	@Override
+	public void crearCliente() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Cliente consultarCliente(String idCliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void actualizarCliente(String apellido1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizarCliente(String apellido1, String apellido2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizarCliente(String nombre1, String nombre2, String apellido1, String apellido2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizarTelefonoCliente(String telefono) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actualizarCorreoCliente(String correo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
