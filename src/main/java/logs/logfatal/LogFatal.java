6ea0c7b5683f91da7af1e5bdf9705c6139e956db
bacriollo19 <42948119+bacriollo19@users.noreply.github.com>
2019-03-27 12:30:18 -0500

    add logger by type

---
+++ src/main/java/logs/logfatal/LogFatal.java
@@ -0,0 +1,18 @@
+package logs.logfatal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import logs.interfaces.Logger;
+
+public class LogFatal implements Logger {
+
+	@Override
+	public void log(String msg, Class<?> clazz) {
+		DOMConfigurator.configure("src/main/resources/dtds/log4j.xml");
+		Log log = LogFactory.getLog(clazz);
+		log.fatal(msg);
+	}
+
+}
--- src/test/java/Tester/LoggerTest.java
+++ src/test/java/Tester/LoggerTest.java
@@ -3,18 +3,18 @@ package Tester;
 import org.junit.Test;
 
 import logs.factory.LoggerFactory;
-import logs.interfaces.Logger;
 
 public class LoggerTest {
 
-	
-	public void testCrearCliente() {
+	@Test
+	public  void testLogs() {
 		LoggerFactory logf = new LoggerFactory();
-		Logger logger = logf.getLogger(LoggerFactory.DEBUG);
-		logger.log("DebugGGGER",getClass());
+		logf.getLogger(LoggerFactory.INFO).log("hola",getClass());
+		logf.getLogger(LoggerFactory.DEBUG).log("KAREN",getClass());
+		logf.getLogger(LoggerFactory.ERROR).log("CRISTHIAN",getClass());
+		logf.getLogger(LoggerFactory.WARN).log("BRAYAN Y KAREN ",getClass());
+		logf.getLogger(LoggerFactory.FATAL).log("se aman",getClass());
+		logf.getLogger(LoggerFactory.TRACE).log("se aman",getClass());
 	}
-	public static void main(String[] args) {
-		LoggerTest lt = new LoggerTest();
-		lt.testCrearCliente();
-	}
+
 }
